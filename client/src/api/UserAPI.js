c438ca7b1e0344ae61d2a628a52099c3f248ea74
Nimesh <42901200+NimeshMadusanka@users.noreply.github.com>
2021-03-02 02:58:15 +0530

    deploy heroku

---
+++ client/src/api/UserAPI.js
@@ -0,0 +1,65 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import swal from 'sweetalert'
+
+function UserAPI(token) {
+    const [isLogged, setIsLogged] = useState(false)
+    const [isAdmin, setIsAdmin] = useState(false)
+    const [cart, setCart] = useState([])
+    const [history, setHistory] = useState([])
+
+    useEffect(() =>{
+        if(token){
+            const getUser = async () =>{
+                try {
+                    const res = await axios.get('/user/infor', {
+                        headers: {Authorization: token}
+                    })
+
+                    setIsLogged(true)
+                    res.data.role === 1 ? setIsAdmin(true) : setIsAdmin(false)
+
+                    setCart(res.data.cart)
+
+                } catch (err) {
+                    swal(err.response.data.msg)
+                }
+            }
+
+            getUser()
+            
+        }
+    },[token])
+
+    
+
+    const addCart = async (product) => {
+        if(!isLogged) return swal("Please login to continue buying")
+
+        const check = cart.every(item =>{
+            return item._id !== product._id
+        })
+
+        if(check){
+            setCart([...cart, {...product, quantity: 1}])
+
+            await axios.patch('/user/addcart', {cart: [...cart, {...product, quantity: 1}]}, {
+                headers: {Authorization: token}
+            })
+
+        }else{
+            swal("This product has been added to cart.")
+        }
+    }
+
+    return {
+        isLogged: [isLogged, setIsLogged],
+        isAdmin: [isAdmin, setIsAdmin],
+        cart: [cart, setCart],
+        addCart: addCart,
+        history: [history, setHistory]
+    }
+}
+
+export default UserAPI
+ 
